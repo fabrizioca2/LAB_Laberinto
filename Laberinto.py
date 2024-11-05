ebaa82eb530e7a5e8b179fd3fade4d14d9f3b9aa
fabrizioca2 <fabriziocago@gmail.com>
2024-11-05 00:44:07 -0500

    Funcion población inicial

--- Laberinto.py
+++ Laberinto.py
@@ -26,4 +26,19 @@ class GeneticAlgorithm:
         self.crossover_rate = crossover_rate
         self.max_generations = max_generations
         self.population = self.initialize_population()
+    
+        def initialize_population(self):
+             """Genera una población inicial de caminos contiguos desde el inicio."""
+                population = []
+                for _ in range(self.population_size):
+                    path = [self.maze.start]
+                    for _ in range(20):  # Aumenta la longitud inicial para explorar más áreas
+                        next_step = self.get_continuous_step(path[-1])
+                        path.append(next_step)
+                    population.append(path)
+                return population
+
+                    
+
+
         
